1eb4065db47457b42e605d86d0d0317dd63eb5ef
KTCreatorBBN <ktcreatorbbn@gmail.com>
2024-10-11 22:05:59 +0530

    test

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
 # print(app)
 # print(__name__)
 
-
+JOBS = [
+  {
+    'id':1,
+    'title':'Data Anlayst',
+    'location':'Bengaluru',
+    'salary':'Rs 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Bengaluru'
+    
+  },
+  {
+    'id':1,
+    'title':'Data Anlayst',
+    'location':'Bengaluru',
+    'salary':'Rs 10,00,000'
+  }
+]
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS,company_name='KTC')
 
+@app.route("/api/jobs")
+def job():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
